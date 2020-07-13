cfb60e7d1492055dc32bb31bebe749c862055f62
yangqi-pc <yangqi@haier.com>
2020-07-13 17:08:10 +0800

    初始化springboot项目

---
+++ src/main/java/com/itpiggy/server/WechatServerApplication.java
@@ -0,0 +1,13 @@
+package com.itpiggy.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itpiggy/server/WechatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itpiggy.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
